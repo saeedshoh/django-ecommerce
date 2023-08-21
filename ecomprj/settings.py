67cdbc54fabc561c4880baf6dde00abfecddafde
Saidshoh Barotov <52253289+saeedshoh@users.noreply.github.com>
2023-08-21 15:10:45 +0500

    run  python manage.py collectstatic comand

--- ecomprj/settings.py
+++ ecomprj/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'taggit',
+    'ckeditor',
     # Custom Apps
     'core.apps.CoreConfig',
     'django_dump_die',
